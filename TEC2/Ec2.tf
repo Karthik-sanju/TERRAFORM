40c20990cd353b9d7eb90f30f1f2a20081c44855
Karthik kumar reddy <88179147+Karthik-sanju@users.noreply.github.com>
2023-06-05 18:01:17 +0530

    Update Ec2.tf

--- TEC2/Ec2.tf
+++ TEC2/Ec2.tf
@@ -1,10 +1,10 @@
 provider "aws" {
   region     = "us-east-2"
-  access_key = "AKIAUD6Q7ZSTZ4HTF7NN"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
 resource "aws_instance" "practice-ec2" {
-  ami                       = "ami-089a545a9ed9893b6"
+  ami                       = ""
   instance_type             = "t2.micro"
  
 }
